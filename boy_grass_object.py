6ad3eba36b7fe7ac132f39c4cf9d973e1e4c5601
heonilha <heonil0304@gmail.com>
2025-09-30 18:17:19 +0900

    Grass, Ball 클래스 틀 작성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,13 +1,36 @@
 from pico2d import *
 import random
+
+class Grass:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+
+class Ball:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+
 def reset_world():
+    global grass,balls
+    grass=Grass()
+    balls=[Ball() for _ in range(20)]
     pass
 
 def update_world():
+    for ball in balls:
+        ball.update()
     pass
 
 
 def render_world():
+    grass.draw()
+    for ball in balls:
+        ball.draw()
     pass
 
 open_canvas()
